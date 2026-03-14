1cb8c2d762d50f7d37922e29c2acf73f7935f94b
martinsllm <lucaslm2010@gmail.com>
2026-03-14 15:27:13 -0300

    Creating permissions using spatie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Permission::firstOrCreate(['name' => 'add']);
+        Permission::firstOrCreate(['name' => 'edit']);
+        Permission::firstOrCreate(['name' => 'delete']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $adminRole->givePermissionTo(['add', 'edit', 'delete']);
+
+        $userRole = Role::firstOrCreate(['name' => 'user']);
+        $userRole->givePermissionTo(['add']);
+
+        $admin = User::factory()->create();
+        $admin->assignRole('admin');
+
+        $user = User::factory(10)->create();
+        $user->each(function ($user) {
+            $user->assignRole('user');
+        });
     }
 }
