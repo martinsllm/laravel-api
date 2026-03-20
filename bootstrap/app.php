2eb4d3238bf0676b4534fadb137cdfa30323539d
martinsllm <lucaslm2010@gmail.com>
2026-03-19 21:20:36 -0300

    Exceptions setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,28 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        //web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        //commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if($request->expectsJson()){
+                return response()->json([
+                    'message' => $e->getMessage()
+                ], 401);
+            }
+            return null;
+        });
     })->create();
